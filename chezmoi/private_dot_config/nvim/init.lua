7f092856fddfe8d171024b89790cde41bfcba23b
Dany Marcoux <git@dmarcoux.com>
2025-11-22 19:56:13 +0100

    Track dotfiles

---
+++ chezmoi/private_dot_config/nvim/init.lua
@@ -0,0 +1,4 @@
+require("config.lazy")
+
+---- TODO: Port my Neovim config from my NixOS dotfiles
+-- https://github.com/dmarcoux/dotfiles-generated-from-nixos-and-home-manager/tree/main/home-manager/neovim/plugins
